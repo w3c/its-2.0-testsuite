294018ba576799dcbee7b9566da83837dd69f4ae
Yves <yves.savourel@gmail.com>
2013-02-18 07:42:23 -0700

    Added XLIFF extraction from inputdata in xliffsamples.

---
+++ its2.0/extractInputToXliff.bat
@@ -0,0 +1,49 @@
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\allowedcharacters -x inputdata\allowedcharacters\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\allowedcharacters -x inputdata\allowedcharacters\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\disambiguation -x inputdata\disambiguation\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\disambiguation -x inputdata\disambiguation\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\domain -x inputdata\domain\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\domain -x inputdata\domain\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\elementswithintext -x inputdata\elementswithintext\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\elementswithintext -x inputdata\elementswithintext\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\externalresource -x inputdata\externalresource\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\externalresource -x inputdata\externalresource\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\idvalue -x inputdata\idvalue\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\idvalue -x inputdata\idvalue\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\localefilter -x inputdata\localefilter\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\localefilter -x inputdata\localefilter\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\localizationnote -x inputdata\localizationnote\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\localizationnote -x inputdata\localizationnote\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\locqualityissue -x inputdata\locqualityissue\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\locqualityissue -x inputdata\locqualityissue\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\locqualityrating -x inputdata\locqualityrating\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\locqualityrating -x inputdata\locqualityrating\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\mtconfidence -x inputdata\mtconfidence\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\mtconfidence -x inputdata\mtconfidence\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\preservespace -x inputdata\preservespace\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\preservespace -x inputdata\preservespace\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\provenance -x inputdata\provenance\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\provenance -x inputdata\provenance\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\storagesize -x inputdata\storagesize\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\storagesize -x inputdata\storagesize\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\terminology -x inputdata\terminology\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\terminology -x inputdata\terminology\html\*html.html -fc okf_itshtml5
+
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\terminology -x inputdata\terminology\xml\*xml.xml
+call ..\..\trunk\deployment\maven\dist_win32-x86\tikal -od xliffsamples\inputdata\terminology -x inputdata\terminology\html\*html.html -fc okf_itshtml5
+
+pause
